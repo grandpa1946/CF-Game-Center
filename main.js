231e7ffdf90e3c25dee93a511d6aa5810509e958
Kiefer <Kieferlin1001@gmail.com>
2023-04-23 19:12:50 -0700

    grrr

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 // main.js
 
+
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const { spawn } = require('child_process');
+const express = require('express');
+const app2 = express();
+const bp = require("body-parser"); //Body Parser
+const cors = require("cors"); //CORS Policy
+
 
 const createWindow = () => {
   // Create the browser window.
@@ -23,7 +29,7 @@ const createWindow = () => {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('./src/index.html')
+  mainWindow.loadFile('./main/index.html')
 
 
 }
@@ -47,3 +53,45 @@ app.on('ready', () => {
   //const child = spawn('cmd.exe', ['/c', 'start']);
   
 });
+
+
+
+//ExpressJS server
+app2.use(bp.json()); //Body Parser
+app2.use(bp.urlencoded({ extended: true })); //Body Parser
+app2.use(cors()); //CORS Policy
+
+
+
+app2.get('/', (req, res) => {
+  res.send({ message: 'Online' })
+})
+
+
+app2.get('/drives', (req, res) => {
+  // Use the 'wmic' command to get information about available drives on a Windows machine
+  exec('wmic logicaldisk get caption, size, freespace', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    // Split the output into lines and remove any empty ones
+    const lines = stdout.trim().split('\r\n').filter((line) => line !== '');
+
+    // Extract the drive information from each line
+    const drives = lines.slice(1).map((line) => {
+      const [drive, size, available] = line.trim().split(/\s+/);
+      return { Drive: drive, DriveSpace: size, AvailableSpace: available };
+    });
+
+    // Send the drive information as JSON
+    res.json({ Drives: drives });
+  });
+});
+
+
+
+
+const port = 3000
+app2.listen(port, () => {})
