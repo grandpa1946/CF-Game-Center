30e1dc89b3c8e79af5bf992a7317343531f3106d
Kiefer <Kieferlin1001@gmail.com>
2023-05-03 16:25:10 -0700

    disabling stuff

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
@@ -26,7 +26,14 @@ const createWindow = () => {
 
     webPreferences: {
       nodeIntegration: true,
+      devTools: false,
     },
+  });
+  // Disable the application menu
+  Menu.setApplicationMenu(null);
+
+  mainWindow.webContents.on("did-navigate", (event, url) => {
+    event.preventDefault();
   });
 
   // and load the index.html of the app.
