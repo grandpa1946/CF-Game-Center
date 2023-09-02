7a8bb7919c24d54fb666781e7bae32d10c9a2204
Kief <67562560+Kief5555@users.noreply.github.com>
2023-09-02 14:24:46 -0700

    Game size

--- main/script.js
+++ main/script.js
@@ -397,13 +397,13 @@ function showDownloadMenu(Gamename, GaneDownload, GameLaunch, GameSize) {
           GameSize = GameSize.trim();
           GameSize = parseFloat(GameSize);
           if (GameSize < 20) {
-            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300"> <span class="w-2 h-2 mr-1 bg-green-500 rounded-full"></span> ${GameSize} </span>`;
+            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300"> <span class="w-2 h-2 mr-1 bg-green-500 rounded-full"></span> ${GameSize} GB </span>`;
           } else if (GameSize >= 20 && GameSize < 40) {
-            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300"> <span class="w-2 h-2 mr-1 bg-yellow-500 rounded-full"></span> ${GameSize} </span>`;
+            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300"> <span class="w-2 h-2 mr-1 bg-yellow-500 rounded-full"></span> ${GameSize} GB </span>`;
           } else if (GameSize >= 40 && GameSize < 98) {
-            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-orange-100 text-orange-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-orange-900 dark:text-orange-300"> <span class="w-2 h-2 mr-1 bg-orange-500 rounded-full"></span> ${GameSize} </span>`;
+            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-orange-100 text-orange-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-orange-900 dark:text-orange-300"> <span class="w-2 h-2 mr-1 bg-orange-500 rounded-full"></span> ${GameSize} GB </span>`;
           } else if (GameSize >= 98) {
-            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-orange-900 dark:text-orange-300"> <span class="w-2 h-2 mr-1 bg-red-500 rounded-full"></span> ${GameSize} </span>`;
+            document.getElementById("downloader-size").innerHTML = `<span class="inline-flex items-center bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300"> <span class="w-2 h-2 mr-1 bg-red-500 rounded-full"></span> ${GameSize} GB </span>`;
           }
         }
         document.getElementById("size1").textContent = "";
